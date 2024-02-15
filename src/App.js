6c1b96afd8679a85586744406d32656778d6f55c
abhilashdurgam454 <durgamabhilash15@gmail.com>
2024-02-15 01:33:02 -0800

    firstprops

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.css';
+import Home from './Home';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Home name='react' />
+
+     
     </div>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = (props) => {
+  return (
+    <div>
+        <h1>this is a propsconcept in {props.name}</h1>
+    </div>
+  )
+}
+
+export default Home
